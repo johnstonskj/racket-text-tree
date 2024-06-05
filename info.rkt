4b166539eef0b87cd240e80114157878bb238ab9
Simon Johnston <johnstonskj@gmail.com>
2024-06-05 07:47:49 -0700

    style: group info values

--- info.rkt
+++ info.rkt
@@ -1,10 +1,12 @@
 #lang info
+
 (define collection "text-tree")
+(define pkg-desc "Functions to write tree-structured data in text form.")
+(define version "1.0")
+(define pkg-authors '(johnstonskj))
+(define license 'Apache-2.0)
+
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "sandbox-lib" "rackunit-lib"))
 (define scribblings '(("scribblings/text-tree.scrbl" ())))
 (define test-omit-paths '("scribblings"))
-(define pkg-desc "Simple interface to output tree-structured data.")
-(define version "1.0")
-(define pkg-authors '(johnstonskj))
-(define license 'Apache-2.0)
